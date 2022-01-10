6ed5b0119e662aa694aeec697cc69c86ec2fa393
kennynuga <95509866+kennynuga@users.noreply.github.com>
2022-01-10 13:05:31 -0500

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,11 +1,11 @@
 resource "aws_s3_bucket" "my_s3_bucket"{
-	  bucket_prefix = var.bucket_prefix
-	  acl    = "var.acl"
+  bucket_prefix = var.bucket_prefix
+  acl    = "var.acl"
 	  
-	  versioning {
-	    enabled = var.versioning
-	  }
+   versioning {
+    enabled = var.versioning
+  }
 	  
-	  tags = var.tags
-	}
+  tags = var.tags
+}
 
