27484430b58bf0180ed355a169f82430bc83ec68
kennynuga <95509866+kennynuga@users.noreply.github.com>
2022-01-10 13:03:00 -0500

    Create s3.tf

---
+++ s3.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "my_s3_bucket"{
+	  bucket_prefix = var.bucket_prefix
+	  acl    = "var.acl"
+	  
+	  versioning {
+	    enabled = var.versioning
+	  }
+	  
+	  tags = var.tags
+	}
+
