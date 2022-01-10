ffbe8003951c5c989b80034bac553a2b4da76513
kennynuga <95509866+kennynuga@users.noreply.github.com>
2022-01-10 13:09:07 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
-	  backend "s3" {
-	    bucket = "my-dev-tf-state-bucket"
-	    key = "main"
-	    region = "us-east-2"
-	    dynamodb_table = "my-dynamodb-table"
-	  }
-	}
+  backend "s3" {
+    bucket = "my-dev-tf-state-bucket"
+    key = "main"
+    region = "us-east-2"
+    dynamodb_table = "my-dynamodb-table"
+  }
+}
 
