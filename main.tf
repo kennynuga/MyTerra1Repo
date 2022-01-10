054747c5f7c63f129d8333153cdb04704370e171
kennynuga <95509866+kennynuga@users.noreply.github.com>
2022-01-10 18:46:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 #Create security group with firewall rules
 resource "aws_security_group" "security_jenkins_grp" {
-  name        = var.security_group
+  name        = "var.security_group"
   description = "security group for jenkins"
 
   ingress {
@@ -30,17 +30,17 @@ resource "aws_security_group" "security_jenkins_grp" {
   }
 
   tags= {
-    Name = var.security_group
+    Name = "var.security_group"
   }
 }
 
 resource "aws_instance" "myFirstInstance" {
-  ami           = var.ami_id
-  key_name = var.key_name
-  instance_type = var.instance_type
+  ami           = "var.ami_id"
+  key_name = "var.key_name"
+  instance_type = "var.instance_type"
   security_groups= [var.security_group]
   tags= {
-    Name = var.tag_name
+    Name = "var.tag_name"
   }
 }
 
